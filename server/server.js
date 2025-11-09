9a6330ac3655a0c41b53001c0fb250a4a4ca285c
eaeoz <sedatergoz@gmail.com>
2025-11-09 03:03:13 +0300

    server cors fixed

--- server/server.js
+++ server/server.js
@@ -21,6 +21,8 @@ const httpServer = createServer(app);
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
+  'http://localhost:4000',
+  'http://localhost:8888',
   'https://netcify.netlify.app',
   process.env.CLIENT_URL
 ].filter(Boolean);
